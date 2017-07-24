feb419d6acea192dc74c64a7dce6efd374bdb2cf
ruby-rails-life <rubyrails.life@gmail.com>
2017-07-24 18:50:37 +0900

    I18n:pluralization

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -5,5 +5,9 @@ class BookController < ApplicationController
   	@books = Book.all
   	@books_published = Book.publised
     @books_order = books
+    I18n.backend.store_translations :en, thanks: 'Thanks %{name}!'
+    I18n.backend.store_translations :ja, thanks: 'ありがとう %{name}!'
+    I18n.backend.store_translations :en, inbox: {one: 'one message', other: '%{count} messages'}
+    I18n.backend.store_translations :ja, inbox: {one: '１件メッセージ', other: '%{count}件メッセージ'}
   end
 end
