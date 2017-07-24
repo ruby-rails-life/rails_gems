d7fed7821b1cd76f2850196667ecf08f6c0ad9c4
ruby-rails-life <rubyrails.life@gmail.com>
2017-07-24 15:46:15 +0900

    kaminari:create friendly URLs

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
   get 'holidays', to: 'holidays#index'
   get 'holidays/edit'
   put 'holidays', to: 'holidays#update'
-  resources :simpleforms
+  #resources :simpleforms
+
+  concern :paginatable do
+    get '(page/:page)', action: :index, on: :collection, as: ''
+  end
+  resources :simpleforms, concerns: :paginatable
+
   resources :topics
   resources :users
   resources :articles do
